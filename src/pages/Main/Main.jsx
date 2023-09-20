2717530e47c111b35731741fc42df31809e629b4
Kiritsugucarl <bluesniper017@gmail.com>
2023-09-20 14:52:59 +0800

    finish: navbar (2 breakpoints) and main (mobile layout only)

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,56 +1,90 @@
+import { useState } from "react";
 import SearchBox from "./components/SearchBox";
 import "./Main.css";
+import testData from "./testData.json";
 
 const Main = () => {
-    let songName = "Trumpets";
-    let songArtist = "Jason Derulo";
-    let songAlbum = "Tattoos";
-    let songAlbumImg =
-        "https://upload.wikimedia.org/wikipedia/en/0/0b/Tattoos_by_Jason_Derulo.jpg";
-    let songRelease = 2013;
+    const [addedSongs, setAddedSongs] = useState([]);
+
+    const addSongToAddedList = (songId) => {
+        const songToAdd = testData.find((song) => song.id === songId);
+        setAddedSongs([...addedSongs, songId]);
+    };
+
+    // const removeSongFromAddedList = (songId) => {
+    //     setAddedSongs(addedSongs.filter((song) => song.id !== songId));
+    // };
+
+    const getSongDetails = (songId) => {
+        return testData.find((song) => song.id === songId);
+    };
 
     return (
         <main className="container">
             <div className="main-wrapper">
-                {/* sidebar */}
-                <div className="main__sidebar">
-                    <h4 className="main__sidebar-title">Added Songs</h4>
-                    <ul className="main__sidebar-list">
-                        <li className="main__sidebar-item">
-                            <p className="main__sidebar-number">1</p>
-                            <img
-                                className="main__sidebar-albumImg"
-                                src={songAlbumImg}
-                            />
-                            <div className="main__sidebar-details-wrapper">
-                                <p className="main__sidebar-details-songName">
-                                    {songName}
+                <div className="main__content-wrapper">
+                    <div className="main__title-wrapper">
+                        <h2 className="main__title">Find your music taste.</h2>
+                        <p className="main__titleDesc">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Vestibulum fringilla elit vel aliquet
+                            fringilla. Nunc luctus sed orci ac sodales.npm
+                        </p>
+                    </div>
+                    <SearchBox addSongToAddedList={addSongToAddedList} />
+                </div>
+                <div className="main__addedSongs-wrapper">
+                    <h2 className="main__addedSong-title">ADDED SONGS</h2>
+                    <ul className="main__addedSong-songList">
+                        {addedSongs.map((songId) => (
+                            <li
+                                key={songId}
+                                className="main__addedSong-songList-item"
+                            >
+                                <p className="main__addedSong-songList-iteration">
+                                    {songId}
                                 </p>
-                                <p className="main__sidebar-details-songDetails">
-                                    {songArtist} - {songAlbum} ({songRelease})
-                                </p>
-                            </div>
-                            <button className="main__sidebar-removeBtn">
-                                X
-                            </button>
-                        </li>
+                                <img
+                                    className="main__addedSong-songList-songAlbumImg"
+                                    src={getSongDetails(songId)?.songAlbumImg}
+                                />
+                                <div className="main__addedSong-songList-details-wrapper">
+                                    <p className="main__addedSong-songList-songName">
+                                        {getSongDetails(songId)?.songName}
+                                    </p>
+                                    <p className="main__addedSong-songList-songDetails">
+                                        {getSongDetails(songId)?.songArtist} -{" "}
+                                        {getSongDetails(songId)?.songAlbum} (
+                                        {getSongDetails(songId)?.songReleased})
+                                    </p>
+                                </div>
+                                <button
+                                    className="main__addedSong-songList-removeButton"
+                                    // onClick={() =>
+                                    //     removeSongFromAddedList(songId)
+                                    // }
+                                >
+                                    <svg
+                                        className="searchResult__closeButtonImg"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.5"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M6 18L18 6M6 6l12 12"
+                                        />
+                                    </svg>
+                                </button>
+                            </li>
+                        ))}
                     </ul>
-                    <button className="main__sidebar-getResultBtn">
+                    <button className="main__addedSongs-getResultBtn">
                         Get Result
                     </button>
-                </div>
-
-                {/* main content */}
-                <div className="main__content">
-                    <h1 className="main__content-title">
-                        Find your music taste.
-                    </h1>
-                    <p className="main__content-desc">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Vestibulum fringilla elit vel aliquet fringilla. Nunc
-                        luctus sed orci ac sodales.
-                    </p>
-                    <SearchBox />
                 </div>
             </div>
         </main>
--- src/pages/Main/components/SearchBox.jsx
+++ src/pages/Main/components/SearchBox.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import testData from "../testData.json";
 
-const SearchBox = () => {
+const SearchBox = ({ addSongToAddedList }) => {
     const [searchInput, setSearchInput] = useState("");
     const [filteredData, setFilteredData] = useState(testData);
+    const [selectedSongData, setSelectedSongData] = useState(null);
+    const [showPopup, setShowPopup] = useState(false);
 
     const handleInputChange = (e) => {
         const { value } = e.target;
@@ -16,6 +18,24 @@ const SearchBox = () => {
             item.songName.toLowerCase().includes(searchInput.toLowerCase())
         );
         setFilteredData(filteredData);
+    };
+
+    const handleItemClick = (item) => {
+        setSelectedSongData(item);
+        setShowPopup(true);
+    };
+
+    const handleClosePopup = () => {
+        setShowPopup(false);
+    };
+
+    const addSong = (item) => {
+        addSongToAddedList(item);
+        console.log(item);
+    };
+
+    const playSong = (item) => {
+        console.log("Playing Song ID: " + item);
     };
 
     return (
@@ -50,8 +70,12 @@ const SearchBox = () => {
                     <li
                         key={item.id}
                         className="main__content-search-result-item"
+                        onClick={() => handleItemClick(item)}
                     >
-                        <button className="main__content-search-result-playButton">
+                        <button
+                            className="main__content-search-result-playButton"
+                            onClick={() => playSong(item)}
+                        >
                             <svg
                                 className="main__content-search-result-play"
                                 xmlns="http://www.w3.org/2000/svg"
@@ -88,6 +112,75 @@ const SearchBox = () => {
                     </li>
                 ))}
             </ul>
+
+            {showPopup && (
+                <div className="searchResult__popup">
+                    <div
+                        className="searchResult__closeSection"
+                        onClick={handleClosePopup}
+                    ></div>
+                    <div className="searchResult__popup-container">
+                        <button
+                            className="searchResult__closeButton"
+                            onClick={handleClosePopup}
+                        >
+                            <svg
+                                className="searchResult__closeButtonImg"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </button>
+                        <div className="searchResult__popup-details">
+                            <img
+                                className="searchResult__popup-img"
+                                src={selectedSongData.songAlbumImg}
+                            />
+                            <div className="searchResult__popup-songDetails-container">
+                                <div className="searchResult__popup-songDetails-title-wrapper">
+                                    <p className="searchResult__popup-title">
+                                        {selectedSongData.songName}
+                                    </p>
+                                    <p className="searchResult__popup-subtitle">
+                                        {selectedSongData.songArtist} -{" "}
+                                        {selectedSongData.songAlbum} (
+                                        {selectedSongData.songReleased})
+                                    </p>
+                                </div>
+                                <button
+                                    className="searchResult__popup-addButton"
+                                    onClick={() => addSong(selectedSongData.id)}
+                                >
+                                    <svg
+                                        className="searchResult__popup-addButtonImg"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        fill="currentColor"
+                                    >
+                                        <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="searchResult__popup-songDetails-songBar">
+                                00:00 Bar to be added later 0:20
+                            </div>
+                            <p className="searchResult__popup-songDetails-tip">
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Vestibulum fringilla elit vel
+                                aliquet fringilla.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
