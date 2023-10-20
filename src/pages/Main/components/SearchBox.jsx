1c768d7b9b07d1fa0a2a0e6e7b6b584084e3f15f
Kiritsugucarl <bluesniper017@gmail.com>
2023-10-20 16:01:13 +0800

    add: search and preview playback

--- src/pages/Main/components/SearchBox.jsx
+++ src/pages/Main/components/SearchBox.jsx
@@ -1,53 +1,87 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import testData from "../testData.json";
-import { TokenProvider, useToken } from "../../../config/TokenContext";
+// import testData from "../testData.json";
+import { useToken } from "../../../config/TokenContext";
+import {
+    CLIENT_ID,
+    REDIRECT_URI,
+    RESPONSE_TYPE,
+} from "../../../config/spotifyConfig";
 import "./SearchBox.css";
 
 const SearchBox = ({ addSongToAddedList }) => {
+    // Use Access Token stored
+    const { token, setToken } = useToken();
+
+    const SEARCH_ENDPOINT = "https://api.spotify.com/v1/search";
+
+    // Searchbox Parts
     const [searchKey, setSearchKey] = useState("");
-    // const [artists, setArtists] = useState("");
-    const [filteredData, setFilteredData] = useState(testData);
+    // const [filteredData, setFilteredData] = useState(testData);
+    const [searchResults, setSearchResults] = useState({
+        albums: { items: [] },
+    });
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalResults, setTotalResults] = useState(0);
+    const [currentPreviewUrl, setCurrentPreviewUrl] = useState("");
+
+    // Popup requirements
     const [selectedSongData, setSelectedSongData] = useState(null);
     const [showPopup, setShowPopup] = useState(false);
 
-    // const { token } = useToken();
+    let searchTimeout;
 
-    // useEffect(() => {
-    //     const searchArtists = async () => {
-    //         if (searchKey) {
-    //             const { data } = await axios.get(
-    //                 "https://api.spotify.com/v1/search",
-    //                 {
-    //                     headers: {
-    //                         Authorization: `Bearer ${token}`,
-    //                     },
-    //                     params: {
-    //                         q: searchKey,
-    //                         type: "artist",
-    //                     },
-    //                 }
-    //             );
-    //             setArtists(data.artists.items);
-    //         } else {
-    //             setArtists([]);
-    //         }
-    //     };
+    const searchItems = async (e) => {
+        e.preventDefault();
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            params: {
+                q: searchKey,
+                type: "track",
+                offset: (currentPage - 1) * 10,
+                limit: 10,
+            },
+        };
 
-    //     searchArtists();
-    // }, [searchKey, token]);
+        try {
+            const response = await axios.get(SEARCH_ENDPOINT, config);
+            console.log(response.data);
+            setSearchResults(response.data);
+            setTotalResults(response.data.total);
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
+    const fetchAlbumImg = async (e) => {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        };
+
+        try {
+            const response = await axios.get(
+                `https://api.spotify.com/v1/albums/${albumId}`,
+                config
+            );
+            return response.data;
+        } catch (error) {
+            console.error("Error fetching album info: ", error);
+            return null;
+        }
+    };
 
     const handleInputChange = (e) => {
         const { value } = e.target;
         setSearchKey(value);
-        filterData(value);
-    };
 
-    const filterData = (searchKey) => {
-        const filteredData = testData.filter((item) =>
-            item.songName.toLowerCase().includes(searchKey.toLowerCase())
-        );
-        setFilteredData(filteredData);
+        console.log(searchKey);
+
+        clearTimeout(searchTimeout); // Clear the previous timeout
+        searchTimeout = setTimeout(() => searchItems(e), 1000); // Pass the event object to searchItems
     };
 
     const handleItemClick = (item) => {
@@ -64,8 +98,77 @@ const SearchBox = ({ addSongToAddedList }) => {
         console.log(item);
     };
 
-    const playSong = (item) => {
-        console.log("Playing Song ID: " + item);
+    const playSong = (previewUrl) => {
+        console.log(previewUrl);
+        setCurrentPreviewUrl(previewUrl);
+    };
+
+    const renderResults = () => {
+        if (!searchResults || !searchResults.tracks) {
+            return (
+                <div>
+                    <p>No items found based...</p>
+                </div>
+            );
+        }
+        return (
+            <ul className="main__content-search-result">
+                {searchResults.tracks.items.map((track) => {
+                    const album = track.album; // Extract album information
+                    const previewUrl = track.preview_url;
+                    {
+                        console.log(`ID: ${track.id}`);
+                    }
+                    return (
+                        <li
+                            key={track.id}
+                            className="main__content-search-result-item"
+                            // onClick = {() => handleItemClick(item)}
+                        >
+                            <button
+                                className="main__content-search-result-playButton"
+                                onClick={() => playSong(previewUrl)}
+                            >
+                                <svg
+                                    className="main__content-search-result-play"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                    />
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+                                    />
+                                </svg>
+                            </button>
+                            {album.images && album.images[0] && (
+                                <img
+                                    className="main__content-search-result-albumImg"
+                                    src={album.images[0].url}
+                                />
+                            )}
+                            <div className="main__content-search-result-songDetails-wrapper">
+                                <p className="main__content-search-result-songName">
+                                    {track.name}
+                                </p>
+                                <p className="main__content-search-result-songDetails">
+                                    {track.artists.map((artist) => artist.name)}
+                                    - {track.album.release_date.substring(0, 4)}
+                                </p>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+        );
     };
 
     return (
@@ -85,131 +188,29 @@ const SearchBox = ({ addSongToAddedList }) => {
                         d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
                     />
                 </svg>
-
-                <input
-                    className="main__content-searchBar"
-                    type="text"
-                    placeholder="Search..."
-                    value={searchKey}
-                    onChange={handleInputChange}
-                />
+                {console.log(token)}
+                {token ? (
+                    <input
+                        className="main__content-searchBar"
+                        type="text"
+                        placeholder="Search..."
+                        onChange={handleInputChange}
+                    />
+                ) : (
+                    <span>ERR 401: No token found</span>
+                )}
             </div>
+            {renderResults()}
 
-            <ul className="main__content-search-result">
-                {filteredData.map((item) => (
-                    <li
-                        key={item.id}
-                        className="main__content-search-result-item"
-                        onClick={() => handleItemClick(item)}
-                    >
-                        <button
-                            className="main__content-search-result-playButton"
-                            onClick={() => playSong(item)}
-                        >
-                            <svg
-                                className="main__content-search-result-play"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                                />
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
-                                />
-                            </svg>
-                        </button>
-                        <img
-                            className="main__content-search-result-albumImg"
-                            src={item.songAlbumImg}
-                        />
-                        <div className="main__content-search-result-songDetails-wrapper">
-                            <p className="main__content-search-result-songName">
-                                {item.songName}
-                            </p>
-                            <p className="main__content-search-result-songDetails">
-                                {item.songArtist} - {item.songAlbum} (
-                                {item.songReleased})
-                            </p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-
-            {showPopup && (
-                <div className="searchResult__popup">
-                    <div
-                        className="searchResult__closeSection"
-                        onClick={handleClosePopup}
-                    ></div>
-                    <div className="searchResult__popup-container">
-                        <button
-                            className="searchResult__closeButton"
-                            onClick={handleClosePopup}
-                        >
-                            <svg
-                                className="searchResult__closeButtonImg"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
-                        </button>
-                        <div className="searchResult__popup-details">
-                            <img
-                                className="searchResult__popup-img"
-                                src={selectedSongData.songAlbumImg}
-                            />
-                            <div className="searchResult__popup-songDetails-container">
-                                <div className="searchResult__popup-songDetails-title-wrapper">
-                                    <p className="searchResult__popup-title">
-                                        {selectedSongData.songName}
-                                    </p>
-                                    <p className="searchResult__popup-subtitle">
-                                        {selectedSongData.songArtist} -{" "}
-                                        {selectedSongData.songAlbum} (
-                                        {selectedSongData.songReleased})
-                                    </p>
-                                </div>
-                                <button
-                                    className="searchResult__popup-addButton"
-                                    onClick={() => addSong(selectedSongData.id)}
-                                >
-                                    <svg
-                                        className="searchResult__popup-addButtonImg"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 24 24"
-                                        fill="currentColor"
-                                    >
-                                        <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
-                                    </svg>
-                                </button>
-                            </div>
-                            <div className="searchResult__popup-songDetails-songBar">
-                                00:00 Bar to be added later 0:20
-                            </div>
-                            <p className="searchResult__popup-songDetails-tip">
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit. Vestibulum fringilla elit vel
-                                aliquet fringilla.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+            {currentPreviewUrl && (
+                <audio
+                    controls
+                    autoPlay
+                    src={currentPreviewUrl}
+                    className="main__content-search-result-audio"
+                >
+                    Your browser does not support the audio element.
+                </audio>
             )}
         </div>
     );
