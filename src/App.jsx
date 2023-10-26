77be93392d746da6eb0e0d5aafea36152b2bdcb4
Carl Dennis Pineda <bluesniper017@gmail.com>
2023-10-26 14:03:36 +0800

    add:home-page (#16)
    Co-authored-by: itsmeDan30 <manoscadan@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Root from "./layout/Root/index.jsx";
 import Home from "./pages/Home/Home.jsx";
+
 import Main from "./pages/Main/Main.jsx";
 import Result from "./pages/Result/Result.jsx";
 import { TokenProvider } from "./config/TokenContext.jsx";
+
+
+
 
 const router = createBrowserRouter([
     {
@@ -14,6 +18,8 @@ const router = createBrowserRouter([
                 path: "/",
                 element: <Home />,
             },
+
+
             {
                 path: "/main",
                 element: <Main />,
