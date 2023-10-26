8c41ce3b7393bbd62ccc115422936bd194736b36
Kiritsugucarl <bluesniper017@gmail.com>
2023-10-26 14:22:03 +0800

    fixed: home > modelImg positioning

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,8 +7,8 @@ import {
 } from "../../config/spotifyConfig";
 import { useToken } from "../../config/TokenContext";
 import "./Home.css";
-import "./spotify-logo.png"
-import "./model.png"
+import "./spotify-logo.png";
+import "./model.png";
 
 const Home = () => {
     const { token } = useToken();
@@ -18,40 +18,39 @@ const Home = () => {
     const navigate = useNavigate();
 
     return (
-            <section id="home">  
-                <div className="home-content">
-                 <h1>
-                    MUSICTASTE.ME
-                 </h1>
-                </div>
-                 
-              <div className="home-paragraph">   
-                 <p className="homeParagraph">
-                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vestibulum 
-                 fringilla <br/> elit vel aliquet fringilla. Nunc luctus sed orci ac sodales. Donec 
-                tincidunt venenatis, <br/> pulvinar mattis sem blandit vitae
+        <section id="home">
+            <div className="home-content">
+                <h1>MUSICTASTE.ME</h1>
+            </div>
+
+            <div className="home-paragraph">
+                <p className="homeParagraph">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit.Vestibulum fringilla <br /> elit vel aliquet fringilla.
+                    Nunc luctus sed orci ac sodales. Donec tincidunt venenatis,{" "}
+                    <br /> pulvinar mattis sem blandit vitae
                 </p>
             </div>
-            <div className="btn-start">
             {!token ? (
-                <a
-                    href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-                > GET STARTED
-                </a>
+                <div className="btn-start">
+                    <a
+                        href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+                    >
+                        {" "}
+                        GET STARTED
+                    </a>
+                </div>
             ) : (
                 navigate("/main")
             )}
-             </div>
-             <div className="powered">
-             <p className="powered-Spotify">
-                Powered by
-             </p>
-             </div>
-             <img src="./spotify-logo.png" alt="Picture" className="logoImg" />
-             <img src="./model.png" alt="Picture" class="modelImg" />
-            
-            </section>
-    )
-}
+
+            <div className="powered">
+                <p className="powered-Spotify">Powered by</p>
+            </div>
+            <img src="./spotify-logo.png" alt="Picture" className="logoImg" />
+            <img src="./model.png" alt="Picture" class="modelImg" />
+        </section>
+    );
+};
 
 export default Home;
