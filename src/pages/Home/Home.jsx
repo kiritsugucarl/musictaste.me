f9f80e4fb98c9a36457d44bc4e58c2ef8bd7ae5f
itsmeDan30 <manoscadan@gmail.com>
2023-10-26 13:15:38 +0800

    add:home-page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,8 @@ import {
 } from "../../config/spotifyConfig";
 import { useToken } from "../../config/TokenContext";
 import "./Home.css";
+import "./spotify-logo.png"
+import "./model.png"
 
 const Home = () => {
     const { token } = useToken();
@@ -16,18 +18,39 @@ const Home = () => {
     const navigate = useNavigate();
 
     return (
-        <main className="container section">
-            <h1>musictaste.me</h1>
+            <section id="home">  
+                <div className="home-content">
+                 <h1>
+                    MUSICTASTE.ME
+                 </h1>
+                </div>
+                 
+              <div className="home-paragraph">   
+                 <p className="homeParagraph">
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vestibulum 
+                 fringilla <br/> elit vel aliquet fringilla. Nunc luctus sed orci ac sodales. Donec 
+                tincidunt venenatis, <br/> pulvinar mattis sem blandit vitae
+                </p>
+            </div>
+            <div className="btn-start">
             {!token ? (
                 <a
                     href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-                >
-                    Login to Spotify
+                > GET STARTED
                 </a>
             ) : (
                 navigate("/main")
             )}
-        </main>
+             </div>
+             <div className="powered">
+             <p className="powered-Spotify">
+                Powered by
+             </p>
+             </div>
+             <img src="./spotify-logo.png" alt="Picture" className="logoImg" />
+             <img src="./model.png" alt="Picture" class="modelImg" />
+            
+            </section>
     )
 }
 
