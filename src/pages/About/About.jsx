eea08b705b1880a1eb4765de12c2870f2310f588
Carl Dennis Pineda <bluesniper017@gmail.com>
2023-11-06 12:20:51 +0800

    Kayla dev (#18)
    * Dev (#7)
    
    * finish navbar (2 br points) and main (mobile layout) (#2)
    
    * tutorial v1
    
    * tutorial v2
    
    * update: navbar
    
    * finish: navbar (2 breakpoints) and main (mobile layout only)
    
    * Spotify integration (#4)
    
    * media query: main, navbar
    
    * add: spotifyAuth, spotifyConfig, tokenHook; fix: segragation of css in MainCss and SearchBox.css
    
    * initial design - result page (#6)
    
    * pre-fix sizes
    
    * apply: search
    
    * Revert "initial design - result page (#6)"
    
    This reverts commit b4d8cc4548f4af10424d692ea436d214575996e0.
    
    * fix: conflicts
    
    ---------
    
    Co-authored-by: Maica <88648097+aciamdesu@users.noreply.github.com>
    
    * fix: image error
    
    * fix: README.md (#14)
    
    * fix: merge errors (#13)
    
    * Dev (#7)
    
    * finish navbar (2 br points) and main (mobile layout) (#2)
    
    * tutorial v1
    
    * tutorial v2
    
    * update: navbar
    
    * finish: navbar (2 breakpoints) and main (mobile layout only)
    
    * Spotify integration (#4)
    
    * media query: main, navbar
    
    * add: spotifyAuth, spotifyConfig, tokenHook; fix: segragation of css in MainCss and SearchBox.css
    
    * initial design - result page (#6)
    
    * pre-fix sizes
    
    * apply: search
    
    * Revert "initial design - result page (#6)"
    
    This reverts commit b4d8cc4548f4af10424d692ea436d214575996e0.
    
    * fix: conflicts
    
    ---------
    
    Co-authored-by: Maica <88648097+aciamdesu@users.noreply.github.com>
    
    * fix: image error
    
    ---------
    
    Co-authored-by: Maica <88648097+aciamdesu@users.noreply.github.com>
    
    * fix: README.md
    
    ---------
    
    Co-authored-by: Maica <88648097+aciamdesu@users.noreply.github.com>
    
    * About and Contact Us page
    
    * fix: merge conflicts
    
    * revision for about_page and contact_page
    
    * improvement pattern instigations on kayla-dev css
    
    * revision for About and Contact Page
    
    ---------
    
    Co-authored-by: Maica <88648097+aciamdesu@users.noreply.github.com>
    Co-authored-by: kcdescalzo <kayladescalzo12@gmail.com>

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {Link } from "react-router-dom";
-import "./About.css"; 
+import { Link } from "react-router-dom";
+import "./About.css";
 import aboutimage from "/about.image.png";
 import member1 from "/member1.png";
 import member2 from "/member2.png";
@@ -9,55 +9,79 @@ import member4 from "/member4.png";
 import member5 from "/member5.png";
 
 const About = () => {
-  return (
-    <div className="about-page">
-      <h1>ABOUT THE WEBSITE</h1>
-      <div className="about-image">
-        <img src={aboutimage} alt="About Us" />
-      </div>
-      <div className="about-description">
-        <p>
-          We know you love music. We know you have favorite artists and songs, genres that you love and hate, 
-          and that you listen to over and over again. We also know that sometimes, when it comes to music,
-          you don't know how much you like something until someone else tells you how much they do - and we want to help!
-        </p>
+    return (
+        <div className="container content-container section">
+            <div className="about-wrapper">
+                <h1 className="about-title">ABOUT THE WEBSITE</h1>
+                <img
+                    className="about-top-image"
+                    src={aboutimage}
+                    alt="About Us"
+                />
+
+                <p className="about-description">
+                    We know you love music. We know you have favorite artists
+                    and songs, genres that you love and hate, and that you
+                    listen to over and over again. We also know that sometimes,
+                    when it comes to music, you don't know how much you like
+                    something until someone else tells you how much they do -
+                    and we want to help!
+                </p>
+
+                <hr className="about__line" />
+
+                <h2 className="about__the-team-title">MEET THE TEAM</h2>
+
+                <div className="about__desc-team">
+                    <p>
+                        The Architects of Innovation: Meet the Extraordinary
+                        Team Behind the App's Development. This app's
+                        development team is a dedicated and creative collection
+                        of individuals who have collaborated to bring it to
+                        reality.
+                    </p>
+                </div>
+                <div className="about__meet-the-team">
+                    <div class="about__team">
+                        <img src={member1} alt="Team Member 1" />
+                        <div class="about__name">
+                            {" "}
+                            CARL DENNIS <br class="NLine"></br> PINEDA{" "}
+                        </div>
+                        <div className="about__role"> Backend Developer</div>
+                    </div>
+                    <div class="about__team">
+                        <img src={member2} alt="Team Member 2" />
+                        <div class="about__name">
+                            MICO JOSHUA<br class="NLine"></br> ORTILLO
+                        </div>
+                        <div className="about__role"> Backend Developer</div>
+                    </div>
+                    <div class="about__team">
+                        <img src={member3} alt="Team Member 3" />
+                        <div class="about__name">
+                            SHALOM JAMAICA<br class="NLine"></br> AVE
+                        </div>
+                        <div className="about__role"> Frontend Developer</div>
+                    </div>
+                    <div class="about__team">
+                        <img src={member4} alt="Team Member 4" />
+                        <div class="about__name">
+                            KAYLA CLAUDINE<br class="NLine"></br> DESCALZO
+                        </div>
+                        <div className="about__role"> Frontend Developer</div>
+                    </div>
+                    <div class="about__team">
+                        <img src={member5} alt="Team Member 5" />
+                        <div class="about__name">
+                            DAN EMMANUEL <br class="NLine"></br> MANOSCA
+                        </div>
+                        <div className="about__role"> Frontend Developer</div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <hr className="section-divider" /> 
-      <div className="the-team-container">
-        <h2>MEET THE TEAM</h2>
-      </div>
-      <div className="desc-team">
-        <p>The Architects of Innovation: Meet the Extraordinary Team Behind the App's Development. This app's development team is a dedicated and creative collection of individuals who have collaborated to bring it to reality.</p>
-      </div>
-      <div className="meet-the-team">
-        <div class="team">
-          <img src={member1} alt="Team Member 1" />
-          <div class="name"> CARL DENNIS  <br class="NLine"></br>     PINEDA </div>
-          <div className="role"> Backend Developer</div>
-        </div>
-        <div class="team">
-          <img src={member2} alt="Team Member 2" />
-          <div class="name">MICO JOSHUA<br class="NLine"></br> ORTILLO</div>
-          <div className="role"> Backend Developer</div>
-        </div>
-        <div class="team">
-          <img src={member3} alt="Team Member 3" />
-          <div class="name">SHALOM JAMAICA<br class="NLine"></br> AVE</div>
-          <div className="role"> Frontend Developer</div>
-        </div>
-        <div class="team">
-          <img src={member4} alt="Team Member 4" />
-          <div class="name">KAYLA CLAUDINE<br class="NLine"></br> DESCALZO</div>
-          <div className="role"> Frontend Developer</div>
-        </div>
-        <div class="team">
-          <img src={member5} alt="Team Member 5" />
-          <div class="name">DAN EMMANUEL <br class="NLine"></br> MANOSCA</div>
-          <div className="role"> Frontend Developer</div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default About;
