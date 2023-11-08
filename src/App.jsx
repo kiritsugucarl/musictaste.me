fcc5901008e6d30c6b632b3fae1186db970a0cd1
Carl Dennis Pineda <bluesniper017@gmail.com>
2023-11-08 21:02:01 +0800

    Python new (#19)
    * new branch, copied musicCollage codes from test branch, musicCollage folder
    
    * updates the app.jsx
    
    * added splashscreen
    
    ---------
    
    Co-authored-by: Mico Joshua <mjoshuaortillo@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,34 @@ import Home from "./pages/Home/Home.jsx";
 
 import Main from "./pages/Main/Main.jsx";
 import Result from "./pages/Result/Result.jsx";
+import Collage from "./musicCollege/musicCollage.jsx"
+import Splashscreen from "./musicCollege/musicCollage.jsx";
 import { TokenProvider } from "./config/TokenContext.jsx";
-import About from "./pages/About/About.jsx";
-import Contact from "./pages/Contact/Contact.jsx";
 
 
+// const [isLoading, setIsLoading] = useState(false);
+// useEffect(() => {
+//     setTimeout(() => {
+//         setIsLoading(false);
+//     }, 5000);
+// }, []);
+
+// const { isLoading, startLoading, stopLoading } = useLoading();
+// const loadingData = async () => {
+//     startLoading();
+//     await new Promise((resolve) => setTimeout(resolve, 2000));
+//     stopLoading();
+// };
 
 const router = createBrowserRouter([
     {
         path: "/",
+        // element: isLoading ? <Splashscreen/> : <Root />, // ito yung line na nag wwhitescreen lang siya
         element: <Root />,
         children: [
             {
                 path: "/",
-                element: <Home />,
+                element:  <Home />,
             },
 
             {
@@ -27,19 +41,19 @@ const router = createBrowserRouter([
             {
                 path: "/result",
                 element: <Result />,
-            },
-            {
-                path: "/about",
-                element: <About />,
-            },
-            {
-                path: "/contact",
-                element: <Contact />,
-            },
-            
+            }
         ],
     },
+    {
+        path: "/collage",
+        element: <Collage />,
+    },
+    // {
+    //     path: "/splashscreen",
+    //     element: <Splashscreen />,
+    // }
 ]);
+
 
 const App = () => {
     return (
