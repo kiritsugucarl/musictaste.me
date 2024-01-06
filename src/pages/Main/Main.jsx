09eb327c442d0f41e2d837a5423f92cec2807532
Kiritsugucarl <bluesniper017@gmail.com>
2024-01-06 15:41:52 +0800

    fix: Main

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -8,10 +8,17 @@ const Main = () => {
         <main className="container content-container section">
             <div className="main-wrapper">
                 <Link to="/music-taste" className="main__link">
-                    MusicTaste
+                    <p className="main__linkTitle">MusicTaste</p>
+                    <p className="main__linkDesc">
+                        Time to know your actual music taste now!
+                    </p>
                 </Link>
+
                 <Link to="/music-personality" className="main__link">
-                    Music Personality
+                    <p className="main__linkTitle">MusicPersonality</p>
+                    <p className="main__linkDesc">
+                        Time to know your music personality now!
+                    </p>
                 </Link>
             </div>
         </main>
