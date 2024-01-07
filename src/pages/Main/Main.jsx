f57682a200c3cdb8f12fb5641a825c541208ed02
Carl Dennis Pineda <bluesniper017@gmail.com>
2024-01-07 13:03:27 +0800

    Feature/music personality (#25)
    * add: new pages ; fix: main.jsx
    
    * refactor: variables in MusicTaste folder
    
    * fix: Main
    
    * create: musicPersonality page; move: searchBox component
    
    * fix: text and minor css

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -8,14 +8,14 @@ const Main = () => {
         <main className="container content-container section">
             <div className="main-wrapper">
                 <Link to="/music-taste" className="main__link">
-                    <p className="main__linkTitle">MusicTaste</p>
+                    <p className="main__linkTitle">Music Taste</p>
                     <p className="main__linkDesc">
                         Time to know your actual music taste now!
                     </p>
                 </Link>
 
                 <Link to="/music-personality" className="main__link">
-                    <p className="main__linkTitle">MusicPersonality</p>
+                    <p className="main__linkTitle">Music Personality</p>
                     <p className="main__linkDesc">
                         Time to know your music personality now!
                     </p>
