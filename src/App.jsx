920702a7eb10d1bca02b1accd62545d0184ce3a0
MikuTaro <mjoshuaortillo@gmail.com>
2024-01-09 21:47:43 +0800

    removing the hr on splashscreen

--- src/App.jsx
+++ src/App.jsx
@@ -44,10 +44,10 @@ const router = createBrowserRouter([
             }
         ],
     },
-    {
-        path: "/collage",
-        element: <Collage />,
-    },
+    // {
+    //     path: "/collage",
+    //     element: <Collage />,
+    // },
     // {
     //     path: "/splashscreen",
     //     element: <Splashscreen />,
