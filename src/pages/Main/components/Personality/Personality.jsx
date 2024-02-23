a4a586bb97d389fae8f1e994a350564e67970b14
Kiritsugucarl <bluesniper017@gmail.com>
2024-02-23 13:32:08 +0800

    sort/audio features

--- src/pages/Main/components/Personality/Personality.jsx
+++ src/pages/Main/components/Personality/Personality.jsx
@@ -1,29 +1,80 @@
+import { useState, useEffect } from "react";
 import "./Personality.css";
 
+const cleanUpAudioFeatures = (audioFeatures) => {
+    const featuresToKeep = [
+        "acousticness",
+        "danceability",
+        "energy",
+        "instrumentalness",
+        "liveness",
+        "loudness",
+        "speechiness",
+        "tempo",
+        "valence",
+    ];
+
+    return audioFeatures.map((feature) => {
+        const cleanedFeature = {};
+
+        featuresToKeep.forEach((property) => {
+            cleanedFeature[property] = feature[property];
+        });
+        return cleanedFeature;
+    });
+};
+
 const Personality = ({ audioFeatures }) => {
+    const [overallAverageFeatures, setOverallAverageFeatures] = useState(null);
+
+    useEffect(() => {
+        if (audioFeatures.length > 0) {
+            const sortedFeatures = cleanUpAudioFeatures(audioFeatures);
+
+            const sumFeatures = sortedFeatures.reduce(
+                (accumulator, feature) => {
+                    for (const key in feature) {
+                        if (isNaN(feature[key])) {
+                            continue;
+                        }
+
+                        if (key in accumulator) {
+                            accumulator[key] += feature[key];
+                        } else {
+                            accumulator[key] = feature[key];
+                        }
+                    }
+                    return accumulator;
+                },
+                {}
+            );
+
+            const overallAverages = {};
+            for (const key in sumFeatures) {
+                overallAverages[key] = sumFeatures[key] / audioFeatures.length;
+            }
+
+            setOverallAverageFeatures(overallAverages);
+        }
+    }, [audioFeatures]);
+
     return (
         <div className="personality">
             <h2 className="personality-title">Your music personality is</h2>
-            <ul>
-                {audioFeatures.map((feature, index) => (
-                    <li key={index}>
-                        <strong>Track {index + 1}:</strong>
-                        <ul>
-                            <li>Acousticness: {feature.acousticness}</li>
-                            <li>Danceability: {feature.danceability}</li>
-                            <li>Energy: {feature.energy}</li>
-                            <li>
-                                Instrumentalness: {feature.instrumentalness}
+            <h2>Overall Average Audio Features</h2>
+            {overallAverageFeatures ? (
+                <ul>
+                    {Object.entries(overallAverageFeatures).map(
+                        ([feature, value]) => (
+                            <li key={feature}>
+                                {feature}: {value.toFixed(2)}
                             </li>
-                            <li>Liveness: {feature.liveness}</li>
-                            <li>Loudness: {feature.loudness}</li>
-                            <li>Speechiness: {feature.speechiness}</li>
-                            <li>Tempo: {feature.tempo}</li>
-                            <li>Valence: {feature.valence}</li>
-                        </ul>
-                    </li>
-                ))}
-            </ul>
+                        )
+                    )}
+                </ul>
+            ) : (
+                <p>No data available</p>
+            )}
         </div>
     );
 };
