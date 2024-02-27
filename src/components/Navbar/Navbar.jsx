c542083be112de2812eb47f3504ebf4eac2c91b3
Kiritsugucarl <bluesniper017@gmail.com>
2024-02-27 15:11:20 +0800

    fix: navbar placement

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -29,28 +29,32 @@ const Navbar = ({ isMobileNavOpen, onMobileMenuToggle }) => {
         <div className="header container">
             {/* Desktop nav */}
             <div className="nav-wrapper">
-                <button
-                    className="nav-menuButton"
-                    onClick={() => onMobileMenuToggle(!isMobileNavOpen)}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                            clipRule="evenodd"
-                        />
-                    </svg>
-                </button>
-
                 <div className="nav__logo-wrapper">
                     <img className="nav-logo" src={logo} />
                     <h2 className="nav-title">
                         mt<span className="title-mustard">.</span>me
                     </h2>
+                </div>
+
+                <div>
+                    <button
+                        className="nav-menuButton"
+                        onClick={() => onMobileMenuToggle(!isMobileNavOpen)}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
+                            />
+                        </svg>
+                    </button>
                 </div>
                 <nav className="desktop-nav">
                     <ul className="desktop-nav-ul">
