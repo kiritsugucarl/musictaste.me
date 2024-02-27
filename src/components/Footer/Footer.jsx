a76864aa5803a9a50619fbde8647af750bee9946
aciamdesu <shalomjamaicaave@gmail.com>
2024-02-27 16:45:55 +0800

    revamp-footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
     return (
         <footer className="footer">
             <p className="footer-text">
-                Created for Capstone Project in Rizal Technological University -
-                Boni Campus
+                COPYRIGHT © musictaste.me
+            </p>
+            <p className="links">
+                <Link className="footer__link" to='/about'>ABOUT</Link>      |      <Link className="footer__link" to='/contact'>CONTACT US</Link>
             </p>
         </footer>
     );
