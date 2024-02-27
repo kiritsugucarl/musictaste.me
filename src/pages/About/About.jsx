dc1dcfbaa8b1b32d9851e808a62771033d9e17bf
Carl Dennis Pineda <bluesniper017@gmail.com>
2024-02-27 17:05:28 +0800

    Revamp/footer (#42)
    * revamp-footer
    
    * fix: file dependency error
    
    * remove: unnecessary deps
    
    * fix: image interdependency
    
    ---------
    
    Co-authored-by: aciamdesu <shalomjamaicaave@gmail.com>

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./About.css";
-import aboutimage from "/about.image.png";
 import member1 from "/member1.png";
 import member2 from "/member2.png";
 import member3 from "/member3.png";
@@ -13,11 +12,6 @@ const About = () => {
         <div className="container content-container section">
             <div className="about-wrapper">
                 <h1 className="about-title">ABOUT THE WEBSITE</h1>
-                <img
-                    className="about-top-image"
-                    src={aboutimage}
-                    alt="About Us"
-                />
 
                 <p className="about-description">
                     We know you love music. We know you have favorite artists
--- src/pages/ErrNotFound/ErrNotFound.jsx
+++ src/pages/ErrNotFound/ErrNotFound.jsx
@@ -17,7 +17,7 @@ const ErrNotFound = () => {
     return (
         <div className="err404">
             <h2 className="err404__title">404 Not Found</h2>
-            <img src="/404.jpg" className="err404__image" />
+            <img src="/404.png" className="err404__image" />
             <p className="err404__desc">
                 Page not found! Redirecting back to home. If the page is not
                 redirected{" "}
