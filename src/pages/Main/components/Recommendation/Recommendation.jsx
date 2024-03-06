64e28b8e750dfe28701e140f7201acb337a512ac
Kiritsugucarl <bluesniper017@gmail.com>
2024-03-06 12:50:34 +0800

    improve: music personality result, main issues, and download overlay

--- src/pages/Main/components/Recommendation/Recommendation.jsx
+++ src/pages/Main/components/Recommendation/Recommendation.jsx
@@ -30,16 +30,10 @@ const Recommendation = ({ imageUrl }) => {
                     >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
                             viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
+                            fill="currentColor"
                         >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-                            />
+                            <path d="M12 1.5a.75.75 0 0 1 .75.75V7.5h-1.5V2.25A.75.75 0 0 1 12 1.5ZM11.25 7.5v5.69l-1.72-1.72a.75.75 0 0 0-1.06 1.06l3 3a.75.75 0 0 0 1.06 0l3-3a.75.75 0 1 0-1.06-1.06l-1.72 1.72V7.5h3.75a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h3.75Z" />
                         </svg>
                     </a>
                 </div>
