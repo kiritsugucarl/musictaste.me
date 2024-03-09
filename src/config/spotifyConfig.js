312a737f0ed443bc65c1bace36e72d6187193ddd
Carl Dennis Pineda <bluesniper017@gmail.com>
2024-03-09 23:15:10 +0800

    update: landing image (#65)

--- src/config/spotifyConfig.js
+++ src/config/spotifyConfig.js
@@ -1,3 +1,3 @@
 export const CLIENT_ID = "76119cfba3a9409fbf5db1c44014b7b3";
-export const REDIRECT_URI = "http://localhost:5173/";
+export const REDIRECT_URI = "http://localhost:5173";
 export const RESPONSE_TYPE = "token";
