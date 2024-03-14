6bb90d3e1f937054f9bde355fd36eca974dc7699
Kiritsugucarl <bluesniper017@gmail.com>
2024-03-15 00:34:56 +0800

    fix: footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,18 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <footer className="footer">
-            <p className="footer-text">
-                COPYRIGHT © musictaste.me
-            </p>
-            <p className="links">
-                <Link className="footer__link" to='/about'>ABOUT</Link>      |      <Link className="footer__link" to='/contact'>CONTACT US</Link>
-            </p>
+            <div className="footer-wrapper container">
+                <p className="footer-text">COPYRIGHT © musictaste.me</p>
+                <p className="links">
+                    <Link className="footer__link" to="/about">
+                        ABOUT
+                    </Link>{" "}
+                    |{" "}
+                    <Link className="footer__link" to="/contact">
+                        CONTACT US
+                    </Link>
+                </p>
+            </div>
         </footer>
     );
 };
