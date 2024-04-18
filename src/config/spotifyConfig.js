00a70668bf2267381c6243c7a2a60d23c42aed89
Kiritsugucarl <bluesniper017@gmail.com>
2024-04-19 05:19:42 +0800

    fix: endpoint url

--- src/config/spotifyConfig.js
+++ src/config/spotifyConfig.js
@@ -1,3 +1,3 @@
 export const CLIENT_ID = "76119cfba3a9409fbf5db1c44014b7b3";
-export const REDIRECT_URI = "https://musictaste.me";
+export const REDIRECT_URI = "https://musictaste.me/";
 export const RESPONSE_TYPE = "token";
